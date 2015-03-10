5257c056f28aaaa40b87f40c15482ecb94a99d14
skylee <lxy314453@163.com>
2015-03-10 17:57:25 +0800

    first commit

---
+++ BackgroundThreadDemo/WGS84TOGCJ02.h
@@ -0,0 +1,13 @@
+//
+//  WGS84TOGCJ02.h
+//  BackgroundThreadDemo
+//
+//  Created by skylee on 15/3/10.
+//  Copyright (c) 2015年 skylee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WGS84TOGCJ02 : NSObject
+
+@end
---
+++ BackgroundThreadDemo/WGS84TOGCJ02.m
@@ -0,0 +1,13 @@
+//
+//  WGS84TOGCJ02.m
+//  BackgroundThreadDemo
+//
+//  Created by skylee on 15/3/10.
+//  Copyright (c) 2015年 skylee. All rights reserved.
+//
+
+#import "WGS84TOGCJ02.h"
+
+@implementation WGS84TOGCJ02
+
+@end
